e38dd4e7fd28b7b5f9f5170926234b69748dab55
Genya55 <nk12mv11033@gmail.com>
2024-09-02 23:19:51 -0500

    deploy1

--- src/App.js
+++ src/App.js
@@ -1,40 +1,60 @@
-import { Bar } from 'react-chartjs-2';
+import React from 'react';
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from 'chart.js';
+import { Line } from 'react-chartjs-2';
+import faker from 'faker';
 
-const data = {
-    // x 軸のラベル
-    labels: ['1 月', '2 月', '3 月', '4 月', '5 月', '6 月', '7 月'],
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+export const options = {
+    responsive: true,
+    plugins: {
+        legend: {
+            position: 'top' as const,
+        },
+        title: {
+            display: true,
+            text: 'Chart.js Line Chart',
+        },
+    },
+};
+
+const labels = ['January', 'February', 'March', 'April', 'May', 'June', 'July'];
+
+export const data = {
+    labels,
     datasets: [
         {
-            label: 'Dataset',
-            // データの値
-            data: [65, 59, 80, 81, 56, 55, 40],
-            // グラフの背景色
-            backgroundColor: [
-                'rgba(255, 99, 132, 0.2)',
-                'rgba(255, 159, 64, 0.2)',
-                'rgba(255, 205, 86, 0.2)',
-                'rgba(75, 192, 192, 0.2)',
-                'rgba(54, 162, 235, 0.2)',
-                'rgba(153, 102, 255, 0.2)',
-                'rgba(201, 203, 207, 0.2)',
-            ],
-            // グラフの枠線の色
-            borderColor: [
-                'rgb(255, 99, 132)',
-                'rgb(255, 159, 64)',
-                'rgb(255, 205, 86)',
-                'rgb(75, 192, 192)',
-                'rgb(54, 162, 235)',
-                'rgb(153, 102, 255)',
-                'rgb(201, 203, 207)',
-            ],
-            // グラフの枠線の太さ
-            borderWidth: 1,
+            label: 'Dataset 1',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+        },
+        {
+            label: 'Dataset 2',
+            data: labels.map(() => faker.datatype.number({ min: -1000, max: 1000 })),
+            borderColor: 'rgb(53, 162, 235)',
+            backgroundColor: 'rgba(53, 162, 235, 0.5)',
         },
     ],
 };
 
-// レンダリング
-export default function Index(): JSX.Element {
-    return <Bar data={data} />;
+export function App() {
+    return <Line options={options} data={data} />;
 }
