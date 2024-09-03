2fa899263721c3296c2516964e15563020f922b4
Genya55 <nk12mv11033@gmail.com>
2024-09-02 23:06:52 -0500

    mod2

--- src/App.js
+++ src/App.js
@@ -1,20 +1,46 @@
 import React from "react";
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-} from "chart.js";
-import { Bar } from "react-chartjs-2";
+import { Line } from "react-chartjs-2";
 
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    BarElement,
-    Title,
-    Tooltip,
-    Legend
-);
+function App() {
+    const labels = ["1 ŒŽ", "2 ŒŽ", "3 ŒŽ", "4 ŒŽ", "5 ŒŽ", "6 ŒŽ"];
+    const graphData = {
+        labels: labels,
+        datasets: [
+            {
+                label: "AŽÐ",
+                data: [65, 59, 60, 81, 56, 55],
+                borderColor: "rgb(75, 192, 192)",
+            },
+            {
+                label: "BŽÐ",
+                data: [60, 55, 57, 61, 75, 50],
+                borderColor: "rgb(75, 100, 192)",
+            },
+        ],
+    };
+
+    const options: {} = {
+        maintainAspectRatio: false,
+    };
+
+    const divStyle: React.CSSProperties = {
+        marginLeft: "auto",
+        marginRight: "auto",
+        margin: "10px",
+        width: "500px",
+    };
+
+    return (
+        <div className="App" style={divStyle}>
+            <Line
+                height={300}
+                width={300}
+                data={graphData}
+                options={options}
+                id="chart-key"
+            />
+        </div>
+    );
+}
+
+export default App;
