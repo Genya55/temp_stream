23221d2a7908dccde5f615cf5dc5643046dd47ee
Genya55 <nk12mv11033@gmail.com>
2024-09-03 00:10:32 -0500

    mod10

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ import { Amplify } from "aws-amplify";
 import { withAuthenticator } from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
-import awsExports from "./aws-exports.js";
+import awsExports from "./aws_exports";
 Amplify.configure(awsExports);
 
 function App({ signOut, user }) {
