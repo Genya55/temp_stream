0d7ce66532a0e266bec30eaa585459efb9ea50c1
Genya55 <nk12mv11033@gmail.com>
2024-09-02 22:51:28 -0500

    deploy

--- src/App.js
+++ src/App.js
@@ -1,13 +1,43 @@
-// App.js
-import React from 'react';
-import { Bar } from 'react-chartjs-2';
-import { chartData, chartOptions } from './chart';
+import { useState } from "react";
+import "./App.css";
+import BarChart from "./components/BarChart";
+import LineChart from "./components/LineChart";
+import PieChart from "./components/PieChart";
+import { UserData } from "./Data";
 
 function App() {
+    const [userData, setUserData] = useState({
+        labels: UserData.map((data) => data.year),
+        datasets: [
+            {
+                label: "Users Gained",
+                data: UserData.map((data) => data.userGain),
+                backgroundColor: [
+                    "rgba(75,192,192,1)",
+                    "#ecf0f1",
+                    "#50AF95",
+                    "#f3ba2f",
+                    "#2a71d0",
+                ],
+                borderColor: "black",
+                borderWidth: 2,
+            },
+        ],
+    });
+
+    // IF YOU SEE THIS COMMENT: I HAVE GOOD EYESIGHT
+
     return (
-        <div>
-            <h1>My Chart</h1>
-            <Bar data={chartData} options={chartOptions} />
+        <div className="App">
+            <div style={{ width: 700 }}>
+                <BarChart chartData={userData} />
+            </div>
+            <div style={{ width: 700 }}>
+                <LineChart chartData={userData} />
+            </div>
+            <div style={{ width: 700 }}>
+                <PieChart chartData={userData} />
+            </div>
         </div>
     );
 }
