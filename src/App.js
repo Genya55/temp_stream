2e130a688a64b7f131cc9bb677e45fdb1ba5c600
Genya55 <nk12mv11033@gmail.com>
2024-09-02 23:49:21 -0500

    mod4

--- src/App.js
+++ src/App.js
@@ -1,15 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Amplify } from "aws-amplify";
+import { withAuthenticator } from "@aws-amplify/ui-react";
+import "@aws-amplify/ui-react/styles.css";
 
-function App() {
+import awsExports from "./aws-exports";
+Amplify.configure(awsExports);
+
+function App({ signOut, user }) {
     return (
         <div className="App">
             <header className="App-header">
                 <img src={logo} className="App-logo" alt="logo" />
-                <h2>Hello React app with AWS</h2>
+                <h2>Hello React with AWS</h2>
+                {user ? (
+                    <>
+                        <h3>éÑÇÕå†å¿ÇéùÇ¡ÇƒÇ¢Ç‹Ç∑ÅF{user.username}</h3>
+                        <button onClick={signOut}>Sign out</button>
+                    </>
+                ) : (
+                    <h3>å†å¿Ç™Ç†ÇËÇ‹ÇπÇÒ</h3>
+                )}
             </header>
         </div>
     );
 }
 
-export default App;
+export default withAuthenticator(App);
